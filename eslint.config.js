9f0dab6f30f935c55e579b018184966a78746a63
Gr1ll <cyril.kurmann@gmail.com>
2024-12-12 09:24:15 +0100

    formatter test

--- eslint.config.js
+++ eslint.config.js
@@ -1,14 +1,22 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
+import globals from 'globals';
+import pluginJs from '@eslint/js';
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
   {
-    languageOptions: { globals: globals.browser },
-    rules: {
-      "no-unused-vars": "error",
-      "prefer-const": "error",
-      eqeqeq: "error",
-    },
+    languageOptions:
+      {
+        globals:
+          globals.browser,
+      },
+    rules:
+      {
+        'no-unused-vars':
+          'error',
+        'prefer-const':
+          'error',
+        eqeqeq:
+          'error',
+      },
   },
 ];
