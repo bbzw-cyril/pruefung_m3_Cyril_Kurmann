25cfb0718f54a4c513231e929479c9a3b9c04599
Gr1ll <cyril.kurmann@gmail.com>
2024-12-12 09:22:13 +0100

    linter test

--- eslint.config.js
+++ eslint.config.js
@@ -3,6 +3,12 @@ import pluginJs from "@eslint/js";
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  { languageOptions: { globals: globals.browser } },
-  pluginJs.configs.recommended,
+  {
+    languageOptions: { globals: globals.browser },
+    rules: {
+      "no-unused-vars": "error",
+      "prefer-const": "error",
+      eqeqeq: "error",
+    },
+  },
 ];
--- index.js
+++ index.js
@@ -1,3 +1,12 @@
 import chalk from "chalk";
 
+let myUnusedVariable = "hello";
+
+const one = 1;
+const two = 2;
+
+if (one == two) {
+  console.log("something is wrong");
+}
+
 console.log(chalk.blue("It works"));
